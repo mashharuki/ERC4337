516b4fe4c06ed2d2dff736d39cc24193226ae396
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-05-02 21:58:10 +0900

    追記

--- 4337-sample/scripts/deploy_SimpleAccountFactory.ts
+++ 4337-sample/scripts/deploy_SimpleAccountFactory.ts
@@ -4,6 +4,8 @@ const main = async function () {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
 
+  console.log('==from  addr=', from )
+
   // デプロイ EntryPoint 
   const EntryPoint = await ethers.getContractFactory("EntryPoint");
   const entryPoint = await EntryPoint.deploy();
@@ -13,7 +15,12 @@ const main = async function () {
   // 自分でEntry Pointコントラクトをデプロして指定する場合はこっち
   // const ret = await simpleAccountFactory.deploy(entryPoint.address);
   // StackUpのBundlerを使用する時はStackUpが用意してくれているEntryPoint Contractを指定した方が安定する。
-  const ret = await simpleAccountFactory.deploy("0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789");
+  const ret = await simpleAccountFactory.deploy(
+    entryPoint.address,
+    {
+      from,
+      gasLimit: 6e6,
+    });
 
   console.log('==SimpleAccountFactory addr=', ret.address)
 }
